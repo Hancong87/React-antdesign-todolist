075aac51d8597a4554320a43c4598be1a4a0a834
hancong <hancong.guan@mastercard.com>
2018-08-25 10:03:45 +0800

    常量作为action，易管理

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
 import store from './store/index.js';
+import {CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM} from './store/actionTypes';
 
 // const data = [
 //     'Racing car sprays burning fuel into crowd.',
@@ -44,7 +45,7 @@ class TodoList extends Component {
     handleInputChange(e) {
 
         const action = {
-            type: 'change_input_value',
+            type: CHANGE_INPUT_VALUE',
             value: e.target.value
         }
 
@@ -60,14 +61,14 @@ class TodoList extends Component {
 
     handleBtnClick() {
         const action = {
-          type: 'add_todo_item'
+          type: ADD_TODO_ITEM
         };
         store.dispatch(action);
     }
 
     handleItemDelete(index) {
         const action = {
-            type: 'delete_todo_item',
+            type: DELETE_TODO_ITEM,
             index
         }
         store.dispatch(action);
