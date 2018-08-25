f6b1a5c2ef5f15be10f5ef07229086ac58d3aabf
hancong <hancong.guan@mastercard.com>
2018-08-25 09:17:55 +0800

    add redux dev tool, and make use of store

--- src/TodoList.js
+++ src/TodoList.js
@@ -16,14 +16,21 @@ class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = store.getState();
-        console.log(this.state);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStorechange = this.handleStorechange.bind(this);
+        store.subscribe(this.handleStorechange);
     }
 
     render() {
         return (
             <div style={{marginTop: '10px', marginLeft: '10px'}}>
-                <Input value={this.state.inputValue} placeholder="todo info" style={{width: '300px', marginRight: '10px'}}/>
-                <Button type="primary">Submit</Button>
+                <Input
+                    value={this.state.inputValue}
+                    placeholder="todo info"
+                    style={{width: '300px', marginRight: '10px'}}
+                    onChange={this.handleInputChange}
+                />
+                <Button type="primary" onClick={this.handleBtnClick}>Submit</Button>
                 <List
                     style={{marginTop: '10px', width: '300px'}}
                     bordered
@@ -32,6 +39,30 @@ class TodoList extends Component {
                 />
             </div>
         )
+    }
+
+    handleInputChange(e) {
+
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+
+        store.dispatch(action);
+
+        console.log(e.target.value);
+    }
+
+    handleStorechange() {
+        console.log('store changed');
+        this.setState(store.getState());
+    }
+
+    handleBtnClick() {
+        const action = {
+          type: 'add_todo_item'
+        };
+        store.dispatch(action);
     }
 }
 
