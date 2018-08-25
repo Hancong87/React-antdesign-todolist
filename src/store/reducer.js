10cdd7a38c9853c819e5b1ece69a530e003895c8
hancong <hancong.guan@mastercard.com>
2018-08-25 12:41:19 +0800

    redux总结

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,6 +6,9 @@ const defaultState = {
 }
 
 // reducer 可以接受state，但是绝不能修改state
+// Reducer必须是纯函数：给定固定的输入，就一定会有固定的输出，而且不会有任何副作用
+// 如果下面new Date，则不是纯函数，一旦函数里面有setTimeout/ajax请求/跟日期相关的内容，都不是纯函数
+// 副作用，指的是对参数的修改，如上面写的"绝不能修改state"
 export default (state = defaultState, action) => {
     if (action.type === CHANGE_INPUT_VALUE) {
         const newState = JSON.parse(JSON.stringify(state));
